86166fc51a3c84468ef67529cbfc0a7a419bf568
Kenneth Bushman <kenneth.bushman@generalassemb.ly>
2021-01-08 17:13:16 -0800

    Update Atrticle.js

--- Atrticle.js
+++ Atrticle.js
@@ -1,38 +1,35 @@
 const mongoose = require('mongoose');
-// const Schema = mongoose.Schema(); // You can pull the schema out of mongoose and use it
+// const Schema = mongoose.Schema;
 
-// Mongoose Data Types
-// Array, Object, String, Buffer, Mixed, Number, Boolean, Date
+// const articleSchema = new Schema()
 
-// Article Schema (Blueprint)
 const articleSchema = new mongoose.Schema({
   title: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  author: {
     type: String,
     required: true,
   },
   body: {
     type: String,
   },
+  author: {
+    type: String,
+  },
+  publishDate: {
+    type: Date,
+    default: Date.now,
+  },
+  hidden: {
+    type: Boolean,
+    default: false,
+  },
   comments: [
     {
-      body: String,
-      commentDate: {
-        type: Date,
-        default: Date.now
-      },
+      commentBody: String,
+      commentDate: Date,
     },
   ],
 });
 
-
-// Article Model
-// The first arg is the name of the model
-// The second arg is the blueprint
 const Article = mongoose.model('Article', articleSchema);
 
 module.exports = Article;
